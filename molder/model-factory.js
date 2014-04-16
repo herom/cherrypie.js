fb698d76a1a6c73b86f59767182a2b37c31e1dcc
MadMG <madmg82@gmail.com>
2014-04-16 14:12:06 +0200

    If the 'namespace' property of the model description is missing, the whole 'origin' object will be used.
    + Correction of the property evaluation if a 'namespace' is given in the model description.
    + Added possibility to access already evaluated properties within computed properties.
    + Added unit tests to test property evaluation with a given namespace and for computed properties.

--- molder/model-factory.js
+++ molder/model-factory.js
@@ -18,23 +18,43 @@ var ModelFactory = {
         var keys = Object.keys(modelDescription),
             namespaceExtractor = this._extractNamespace,
             preparedOrigin = {},
-            parsedOutput = {};
+            parsedOutput = {},
+            computedProperties = [];
 
-        preparedOrigin = namespaceExtractor(origin, modelDescription.namespace);
+        if (modelDescription.hasOwnProperty('namespace')) {
+            preparedOrigin = namespaceExtractor(origin, modelDescription.namespace);
+        } else {
+            preparedOrigin = origin;
+        }
 
         keys.forEach(function (key) {
             var value = modelDescription[key],
+                includeProperty = true,
                 parsedValue;
 
-            if(typeof value === 'function') {
-                parsedValue = value(origin, namespaceExtractor);
+            if (typeof value === 'function') {
+                // keep computed properties to evaluate them after the ordinary properties has been evaluated.
+                // the evaluated properties will be accessible within the property function
+                computedProperties.push(key);
+
             } else {
-                if(key.indexOf('serializable') < 0) {
-                    parsedValue = namespaceExtractor(origin, value);
+                includeProperty = key.indexOf('serializable') < 0 && key.indexOf('namespace') < 0;
+                if (includeProperty) {
+                    if (preparedOrigin.hasOwnProperty(value)) {
+                        parsedValue = preparedOrigin[value];
+                    } else {
+                        parsedValue = namespaceExtractor(preparedOrigin, value);
+                    }
+                }
+                if (includeProperty) {
+                    parsedOutput[key] = parsedValue;
                 }
             }
+        });
 
-            parsedOutput[key] = parsedValue;
+        computedProperties.forEach(function (key) {
+            var fn = modelDescription[key];
+            parsedOutput[key] = fn.call(preparedOrigin, preparedOrigin, namespaceExtractor);
         });
 
         return parsedOutput;
@@ -52,11 +72,11 @@ var ModelFactory = {
     _extractNamespace: function (origin, namespace) {
         var namespacedOrigin = null;
 
-        if(typeof namespace === 'string' && namespace.indexOf('.') > -1) {
-           var splittedNamespace =  namespace.split('.'),
-               nestedOrigin = Object.create(origin);
+        if (typeof namespace === 'string' && namespace.indexOf('.') > -1) {
+            var splittedNamespace = namespace.split('.'),
+                nestedOrigin = Object.create(origin);
 
-            splittedNamespace.forEach(function getNamespaceExtractedOrigin (namespaceKey) {
+            splittedNamespace.forEach(function getNamespaceExtractedOrigin(namespaceKey) {
                 nestedOrigin = nestedOrigin[namespaceKey];
             });
 
